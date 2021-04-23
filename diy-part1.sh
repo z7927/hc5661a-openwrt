0d6c6397983db1cafd51c98f91ad3c2fe51c47b8
z7927 <78355455+z7927@users.noreply.github.com>
2021-04-24 00:56:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/Zxilly/UA2F.git
+git clone https://github.com/CHN-beta/rkp-ipid.git
